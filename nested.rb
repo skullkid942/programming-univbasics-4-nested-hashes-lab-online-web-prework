fc645a1586b380d056d32865480cbd47d6631777
Bethany Holley <chocolatechipfaced@gmail.com>
2019-09-30 00:01:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,7 +61,7 @@ end
 end
 
 def adding_matz
-  programmer_hash[:yukihiro_matsumoto]
+  programmer_hash[:yukihiro_matsumoto] => {:known_for => "Ruby", :languages => ["LISP","C"]}
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
