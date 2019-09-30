5256ea25a5d88e8562bca25d3856f8ba55e364dd
Bethany Holley <chocolatechipfaced@gmail.com>
2019-09-30 00:06:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -41,7 +41,7 @@ end
 
 def dennis_ritchies_language
   programmer_hash[:dennis_ritchie][:languages]
-
+end
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -104,7 +104,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     programmer_hash[:alan_kay][:known_for] = ["GUI"]
+     programmer_hash[:alan_kay][:known_for] = [alans_new_info]
      
      
 end
